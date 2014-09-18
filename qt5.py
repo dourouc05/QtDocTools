d66d6fa0b096dae0f3db78e6dbf7b8e8b7c38ef9
Thibaut Cuvelier <tcuvelier@redaction-developpez.com>
2014-09-18 16:15:56 +0200

    Trying to understand things... Far from the point where this script will generate any HTML file!

--- qt5.py
+++ qt5.py
@@ -2,42 +2,74 @@ __author__ = 'Thibaut Cuvelier'
 
 import os, os.path, subprocess
 
-sources = "C:\\Qt\\5.3\\Src\\"
-qdoc = "C:\\Qt\\5.3\\mingw482_32\\bin\\qdoc.exe"
-output = "C:\\Qt\\_script\\html\\"
-conf = "C:\\Qt\\5.3\\Src\\qtbase\\src\\corelib\\doc\\qtcore.qdocconf"
+sources = "C:/Qt/5.3/Src/"
+qdoc = "C:/Qt/5.3/mingw482_32/bin/qdoc.exe"
+output = "C:/Qt/_script/html/"
+# conf = "C:/Qt/5.3/Src/qtbase/src/corelib/doc/qtcore.qdocconf"
 
 environment = {"QT_INSTALL_DOCS": sources + "qtbase/doc", "QT_VERSION_TAG": "531", "QT_VER": "5.3", "QT_VERSION": "5.3.1"}
-
-#subprocess.call([qdoc, "-version"], env=environment)
 
 # Modules to ignore.
 ignored = ["qttranslations"]
 
-# Retrieve the list of modules in the sources.
-modules = []
+# Retrieve the list of modules in the sources, qtbase being the first one.
+modules = ["qtbase", "qtdoc", "qmake"]
 for module in os.listdir(sources):
-    if module.startswith('q') and not module.endswith('.pro') and os.path.isdir(sources + module):
+    if module.startswith('q') and os.path.isdir(sources + module) and module != "qtbase": # and not module.endswith('.pro')
         modules.append(module)
 
 # First, create the indexes by going through all source directories.
+# This requires to first handle Qt Base, to generate its index, then to give it as an argument to all the others.
 def prepareModule(path, outputDirectory, installDirectory, conf):
-    # for sub in os.listdir(path):
-    #     subdir = path + "/" + sub
-    #     if os.path.isdir(subdir):
-    #         prepareModule(subdir, outputDirectory, installDirectory, conf)
+    print("  " + path)
 
-    subprocess.call([qdoc, "-outputdir", outputDirectory, "-installdir", installDirectory, conf], env = environment) # , "-prepare", "-no-link-errors"
+    os.chdir(path)
+    params = [qdoc,
+              "-outputdir", outputDirectory,
+              "-installdir", installDirectory,
+              conf,
+              "-prepare", "-no-link-errors"]
+
+    if "qtbase" not in path:
+        params.extend(["-indexdir", sources + "qtbase/doc"])
+
+    subprocess.call(params, env = environment)
+
+# In order to create the indexes, retrieve the list of configuration files.
+configs = {}
 
 for module in modules:
     path = sources + module + "/"
 
     if module in ignored:
         pass
-    elif module == "qtbase" or module == "qtdoc":
-        print(module)
-    elif os.path.isdir(path + "src"): #  or os.path.isdir(path + "doc"):
-        print("Handling preparation of: " + module)
-        prepareModule(path = path + "src/", outputDirectory = output + module + "/", installDirectory = output, conf = conf)
+    elif module == "qtbase": # QMake is hosted in Qt Base.
+        print("Handling preparation of QMake; path: " + path)
+        for dirpath, dirnames, files in os.walk(path):
+            for file in files:
+                if file.lower().endswith(".qdocconf"):
+                    print("-- " + file)
+    elif module == "qmake": # QMake is hosted in Qt Base.
+        path = sources + "qtbase/qmake"
+        print("Handling preparation of QMake; path: " + path)
+        for dirpath, dirnames, files in os.walk(path): # Don't look in src/!
+            for file in files:
+                if file.lower().endswith(".qdocconf"):
+                    print("-- " + file)
+    #elif module == "qtbase" or module == "qtdoc":
+    #    print(module)
+    #elif os.path.isdir(path + "src"): #  or os.path.isdir(path + "doc"):
     else:
-        print("Problem: unknown structure for " + module)
+        print("Handling preparation of: " + module + "; path: " + path)
+
+        # Find all documentation configuration files.
+        for dirpath, dirnames, files in os.walk(path):
+            for file in files:
+                if file.lower().endswith(".qdocconf"):
+                    print("-- " + file)
+
+
+        conf = sources + module + "/" # "qtbase/src/corelib/doc/qtcore.qdocconf"
+        #prepareModule(path = path, outputDirectory = output + module + "/", installDirectory = output, conf = conf) #  + "src/"
+    #else:
+    #    print("Problem: unknown structure for " + module)
