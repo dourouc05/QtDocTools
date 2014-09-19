8df7f6f5162553a304cf5fd1afed366e89c8719a
Thibaut Cuvelier <tcuvelier@redaction-developpez.com>
2014-09-19 16:37:53 +0200

    This script compiles Qt 5.3's documentation as HTML!

--- qt5.py
+++ qt5.py
@@ -20,23 +20,6 @@ for folder in os.listdir(sources):
     if folder.startswith('q') and os.path.isdir(sources + folder): # and not module.endswith('.pro')
         folders.append(folder)
 
-# First, create the indexes by going through all source directories.
-# This requires to first handle Qt Base, to generate its index, then to give it as an argument to all the others.
-#def prepareModule(path, outputDirectory, installDirectory, conf):
-#    print("  " + path)
-#
-#    os.chdir(path)
-#    params = [qdoc,
-#              "-outputdir", outputDirectory,
-#              "-installdir", installDirectory,
-#              conf,
-#              "-prepare", "-no-link-errors"]
-#
-#    if "qtbase" not in path:
-#        params.extend(["-indexdir", sources + "qtbase/doc"])
-#
-#    subprocess.call(params, env = environment)
-
 # In order to create the indexes, retrieve the list of configuration files.
 configs = {}
 
@@ -53,12 +36,14 @@ for folder in folders:
         if not os.path.isfile(configs["qmake"]):
             print("ERROR: QMake's qdocconf not found!")
 
-        # Then deal with the modules in Qt Base. Don't do a bruteforce search on *.qdocconf as it takes a while.
+        # Then deal with the modules in Qt Base. Don't do a brute force search on *.qdocconf as it takes a while.
         dirs = os.listdir(path + "src/")
         # Filter out folders to ignore; tools are handled separately.
         dirs = [x for x in dirs if x not in qtbaseignore and x != "tools"]
         # Filter out files (like src.pro, the project file).
         dirs = [x for x in dirs if os.path.isdir(path + "src/" + x)]
+
+        # For each Qt Base module, look for documentation configuration files.
         for module in dirs:
             prefixedModule = "qtcore" if module == "corelib" else "qt" + module
 
@@ -83,16 +68,10 @@ for folder in folders:
                         break
                 else:
                     print("ERROR: Qt Base tool " + tool + "'s qdocconf not found!")
-
-        #for dirpath, dirnames, files in os.walk(path + "src/"):
-        #    for file in files:
-        #        if file.lower().endswith(".qdocconf"):
-        #            print("-- " + file)
-    #elif module == "qtbase" or module == "qtdoc":
-    #    print(module)
-    #elif os.path.isdir(path + "src"): #  or os.path.isdir(path + "doc"):
-    else:
+    else: # Other modules.
         print("Handling preparation of: " + folder + "; path: " + path)
+
+        hasSomething = False
 
         # If the folder has a doc/ subfolder, it may contain a qdocconf file. Qt Sensors 5.3 has such a file, but
         # it should not be used (prefer the one in src/). This case is mainly for documentation-only modules,
@@ -101,7 +80,7 @@ for folder in folders:
             docPath = path + "doc/"
 
             # qtwebkit-examples is the name of the folder, but the file is named qtwebkitexamples.qdoc!
-            # Enginio seems to have other idiosyncracies...
+            # Enginio seems to have other idiosyncracies... (documentation both in src/ and doc/).
             docFiles = [docPath + folder + ".qdocconf", docPath + "config/" + folder + ".qdocconf",
                         docPath + folder.replace("-", "") + ".qdocconf",
                         docPath + "config/" + folder.replace("-", "") + ".qdocconf"]
@@ -109,19 +88,27 @@ for folder in folders:
             for docFile in docFiles:
                 if os.path.isfile(docFile):
                     configs[folder] = docFile
+                    hasSomething = True
                     break
+                    # @TODO: May it happen there is more than one file?
+
+        # Second try: the sources folder (either src/ or Source/).
         if os.path.isdir(path + "src/"): # Not exclusive! Otherwise, some documentation is lost (Enginio, Quick 1).
             srcPath = path + "src/"
 
-            # This loop is quite slow, but there is too much variety in those modules...
-            found = False # Is (at least) a file found?
+            # This loop is quite slow, but there is too much variety in those modules (such as a folder containing
+            # multiple modules).
+            # One more strange thing with Qt Multimedia: a qtmultimedia-dita.qdocconf file.
+            found = False # Is (at least) one file found?
             for dirpath, dirnames, files in os.walk(srcPath):
                 for file in files:
-                    if file.endswith(".qdocconf") and "global" not in dirpath:
+                    if file.endswith(".qdocconf") and "global" not in dirpath and "-dita" not in file:
                         configs[file.replace(".qdocconf", "")] = dirpath.replace("\\", "/") + "/" + file
                         found = True
             if not found:
                 print("ERROR: module " + folder + "'s qdocconf not found!")
+            else:
+                hasSomething = True
         elif os.path.isdir(path + "Source/"): # This one is for WebKit...
             srcPath = path + "Source/"
 
@@ -134,16 +121,54 @@ for folder in folders:
                         found = True
             if not found:
                 print("ERROR: module " + folder + "'s qdocconf not found!")
-        ## Find all documentation configuration files.
-        #for dirpath, dirnames, files in os.walk(path):
-        #    for file in files:
-        #        if file.lower().endswith(".qdocconf"):
-        #            print("-- " + file)
+            else:
+                hasSomething = True
 
-
-        conf = sources + folder + "/" # "qtbase/src/corelib/doc/qtcore.qdocconf"
-        #prepareModule(path = path, outputDirectory = output + module + "/", installDirectory = output, conf = conf) #  + "src/"
-    #else:
-    #    print("Problem: unknown structure for " + module)
+        # Final check: there should be something in this directory (otherwise, add it in the ignore list).
+        if not hasSomething:
+            print("ERROR: module " + folder + " completely unrecognised.")
 
 print(configs)
+# Second stage: prepare the documentation, the indexes. Qt Core must be done first, the order has no importance for
+# the others.
+
+# First, create the indexes by going through all source directories.
+# This requires to first handle Qt Core, to generate its index, then to give it as an argument to all the others.
+# @TODO: Triple check whether Qt Core must be done first.
+# @TODO: Seek for parallelisation when running qdoc to fully use multiple cores.
+def prepareModule(moduleName, conf):
+    params = [qdoc,
+              "-outputdir", output + moduleName + "/",
+              "-installdir", output,
+              conf,
+              "-prepare",
+              "-indexdir", output,
+              "-no-link-errors",
+              "-log-progress"]
+
+    print(params)
+    subprocess.call(params, env = environment)
+
+prepareModule(moduleName = "qtcore", conf = configs["qtcore"])
+for moduleName, conf in configs.items():
+    if moduleName == "qtcore":
+        continue
+
+    prepareModule(moduleName = moduleName, conf = conf)
+
+# Eventually start building things.
+def generateModule(moduleName, conf):
+    params = [qdoc,
+              "-outputdir", output + moduleName + "/",
+              "-installdir", output,
+              conf,
+              "-generate",
+              "-indexdir", output,
+              "-no-link-errors",
+              "-log-progress"]
+
+    print(params)
+    subprocess.call(params, env = environment)
+
+for moduleName, conf in configs.items():
+    generateModule(moduleName = moduleName, conf = conf)
