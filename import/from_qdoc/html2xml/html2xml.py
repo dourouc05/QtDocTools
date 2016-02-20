889a1f381f94729ba8d5afe7c13f0341799e5668
Thibaut Cuvelier <dourouc05@gmail.com>
2016-02-20 21:28:28 +0100

    QDoc to DocBook #5: import HTML as XML with standard inputs/outputs, easier to work with XProc (p:exec).

--- import/from_qdoc/html2xml/html2xml.py
+++ import/from_qdoc/html2xml/html2xml.py
@@ -11,21 +11,10 @@ path = ['qtdoc', 'accessibility']
 
 
 def convert(in_file_name, out_file_name):
-    # Read the input file.
     with open(in_file_name, "rb") as f:
         tree = html5lib.parse(f)
-
     with open(out_file_name, 'wb') as f:
         f.write(ET.tostring(tree))
-
-    # # Find the relevant section in it.
-    # ns = {'html': '{http://www.w3.org/1999/xhtml}'}
-    # tree = tree.findall('.//{http://www.w3.org/1999/xhtml}div[@class="content mainContent"]')
-    #
-    # # Build the output document.
-    # out = ET.Element('{http://docbook.org/ns/docbook}article')
-    #
-    # print(tree)
 
 
 convert(input_directory + '/' + '/'.join(path) + input_extension, output_directory + '/' + '/'.join(path) + output_extension)
