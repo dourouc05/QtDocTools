56a014365b141197daf8b71e220b54a66ed2fb88
Thibaut Cuvelier <dourouc05@gmail.com>
2016-05-17 08:39:34 +0200

    QDoc to DocBook #5: test suite, don't run for compat, members, obsolete.

--- import/from_qdoc/xslt/test_5.4/_run.bat
+++ import/from_qdoc/xslt/test_5.4/_run.bat
@@ -1,19 +1,27 @@
 @echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
+setlocal enabledelayedexpansion
 
 SET SAXON=F:/QtDoc/QtDoc/SaxonHE9-7-0-3J/saxon9he.jar
 SET XSLTF=F:/QtDoc/QtDoc/QtDocTools/import/from_qdoc/xslt/qdoc2db_5.4.xsl
 
-echo Starting!
+echo Starting
 for %%f in (*.xml) do (
     set FILENAME=%%~nf
-    echo -- !FILENAME!
-    java -jar %SAXON% -s:!FILENAME!.xml -xsl:%XSLTF% -o:!FILENAME!.db
+    
+    echo !FILENAME! | findstr /C:-compat >nul
+    set ISCOMPAT=!ERRORLEVEL!
+    echo !FILENAME! | findstr /C:-members >nul
+    set ISMEMBER=!ERRORLEVEL!
+    echo !FILENAME! | findstr /C:-obsolete >nul
+    set ISOBSOLETE=!ERRORLEVEL!
+    
+    IF /I "!ISCOMPAT!" EQU "1" (
+        IF /I "!ISMEMBER!" EQU "1" (
+            IF /I "!ISOBSOLETE!" EQU "1" (
+                echo -- !FILENAME!
+                java -jar %SAXON% -s:!FILENAME!.xml -xsl:%XSLTF% -o:!FILENAME!.db
+            )
+        )
+    )
 )
-for %%f in (*.xml) do (
-    set FILENAME=%%~nf
-    if exist !FILENAME!-compat.db   del !FILENAME!-compat.db
-    if exist !FILENAME!-members.db  del !FILENAME!-members.db
-    if exist !FILENAME!-obsolete.db del !FILENAME!-obsolete.db
-)
-echo Done!
+echo Done
