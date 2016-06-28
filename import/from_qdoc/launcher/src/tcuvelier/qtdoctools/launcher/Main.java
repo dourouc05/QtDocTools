0646f0e6feb73854642aad7516ee8b397fbbc4e4
Thibaut Cuvelier <dourouc05@gmail.com>
2016-06-28 14:56:08 +0200

    QDoc to DocBook: launcher for Saxon.

--- import/from_qdoc/launcher/src/tcuvelier/qtdoctools/launcher/Main.java
+++ import/from_qdoc/launcher/src/tcuvelier/qtdoctools/launcher/Main.java
@@ -1,23 +1,17 @@
 package tcuvelier.qtdoctools.launcher;
 
-
-import javax.xml.transform.Templates;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
 
 public class Main {
     // Saxon HE: run with -Djava.xml.transform.TransformerFactory=net.sf.saxon.TransformerFactoryImpl.
@@ -27,99 +21,71 @@ public class Main {
     private static final String extension = ".xml"; // Extension for files that are recognised here.
     private static final String[] forbiddenSuffixes = { "-members", "-compat", "-obsolete" };
     private static final String[] ignoredSuffixes = { "-manifest" };
+    private static final Map<String, String[]> ignoredFiles = new HashMap<>();
+
+    static {
+        ignoredFiles.put("qtdoc", new String[] {
+                "classes.xml", "obsoleteclasses.xml", "hierarchy.xml", "qmlbasictypes.xml", "qmltypes.xml"
+        });
+    }
 
     // Arguments: XSLT file, folder.
     public static void main(String[] args) throws TransformerConfigurationException, IOException {
-        args = new String[] { ".xsl", "F:\\QtDoc\\output\\html\\qtgui"};
+        args = new String[] { "F:\\QtDoc\\QtDoc\\QtDocTools\\import\\from_qdoc\\xslt\\qdoc2db_5.4.xsl", "F:\\QtDoc\\output\\html\\qtdoc2"};
 
         // Parse the command line.
         if (args.length != 2) {
-            System.err.println("Usage: xsl folder");
+            throw new RuntimeException("Usage: xsl folder");
         }
         String xslt = args[0];
         String dir = args[1];
-        if (! xslt.endsWith(".xsl")) {
-            System.err.println("Unrecognised style sheet extension: " + xslt);
+        Path folder = Paths.get(dir);
+        String moduleName = folder.getFileName().toString();
+        if (! xslt.endsWith(".xsl") || xslt.length() == 4) {
+            throw new RuntimeException("Unrecognised style sheet name: " + xslt);
+        }
+        if (! Files.exists(Paths.get(xslt))) {
+            throw new IOException("Style sheet does not exist: " + xslt);
+        }
+        if (! Files.exists(folder)) {
+            throw new IOException("Input folder does not exist: " + dir);
         }
 
         // Compile the style sheet.
-//        Templates templates = TransformerFactory.newInstance().newTemplates(new StreamSource(xslt));
-//        Transformer transformer = templates.newTransformer();
+        Templates templates = TransformerFactory.newInstance().newTemplates(new StreamSource(xslt));
+        Transformer transformer = templates.newTransformer();
 
         // Iterate through the given folder.
-//        File folder = new File(dir);
-//        File[] listing = folder.listFiles();
-//        if (listing == null) {
-//            throw new FileNotFoundException("The given folder has no file: " + dir);
-//        }
-
         String[] folderSuffixes = Stream.concat(Arrays.stream(forbiddenSuffixes), Arrays.stream(ignoredSuffixes))
                                         .map(s -> s + extension)
                                         .toArray(String[]::new);
-        Files.walk(Paths.get(dir))
+        Files.walk(folder)
              .filter(Files::isRegularFile)
              .filter(path -> path.getFileName().toString().endsWith(extension))
              .filter(path -> Arrays.stream(folderSuffixes)
                                    .map(suffix -> !path.getFileName().toString().endsWith(suffix))
                                    .reduce(true, (a, b) -> a && b))
+             .filter(path -> {
+                 if (ignoredFiles.containsKey(moduleName)) {
+                     return Arrays.stream(ignoredFiles.get(moduleName))
+                                  .map(suffix -> !path.getFileName().toString().equals(suffix))
+                                  .reduce(true, (a, b) -> a && b);
+                 } else {
+                     return true;
+                 }
+             })
              .forEach(path -> {
-                 System.out.println(path);;
+                 String filename = path.getFileName().toString();
+                 File in = path.toFile();
+                 File out = path.resolveSibling(filename.replace(".xml", ".db")).toFile();
+
+                 // XSLT transformation.
+                 try {
+                     transformer.transform(new StreamSource(in), new StreamResult(out));
+                 } catch (TransformerException e) {
+                     System.err.println("Problem(s) with file '" + path.getFileName().toFile() + "' at stage XSLT: \\n");
+                     e.printStackTrace();
+                 }
              });
-
-//        for (File child : listing) {
-//            String name = child.getName();
-//            if (! name.endsWith(extension)) {
-//                continue;
-//            }
-//        }
-
-        /*
-        count_db = 0
-
-        for root, sub_dirs, files in os.walk(self.folders['output'] + module_name + '/'):
-            if root.endswith('/style') or root.endswith('/scripts') or root.endswith('/images'):
-                continue
-
-            count = 0
-            n_files = len(files)
-            for file in files:
-                count += 1
-
-                # Handle a bit of output (even though the DocBook counter is not yet updated for this iteration).
-                if count % 10 == 0:
-                    logging.info('XML to DocBook: module %s, %i files done out of %i (%i DocBook files generated)'
-                                 % (module_name, count, n_files, count_db))
-
-                # Avoid lists of examples (-manifest.xml) and files automatically included within the output
-                # with the XSLT stylesheet (-members.xml, -obsolete.xml, -compat.xml).
-                if not file.endswith(ext):
-                    continue
-                if any([file.endswith(fs + ext) for fs in ignored_suffixes]):
-                    continue
-                if any([file.endswith(fs + ext) for fs in forbidden_suffixes]):
-                    continue
-                if module_name in self.ignores['files'] and file in self.ignores['files'][module_name]:
-                    continue
-                count_db += 1
-
-                # Actual processing.
-                base_file_name = os.path.join(root, file[:-4])
-                in_file_name = base_file_name + '.xml'
-                out_file_name = base_file_name + '.db'
-                self._call_xslt(in_file_name, out_file_name)
-
-                # For C++ classes, also handle the function prototypes with the C++ application.
-                if file.startswith('q') and not file.startswith('qml-'):
-                    self._call_cpp_parser(out_file_name, out_file_name)
-
-        return count_db
-         */
-
-//        System.out.println("\n\n----- transform of " + sourceID1 + " -----");
-//        transformer.transform(new StreamSource(sourceID1),
-//                new StreamResult(System.out));
-//        System.out.println("\n\n----- transform of " + sourceID2 + " -----");
-//        transformer.transform(new StreamSource(sourceID2),
-//                new StreamResult(System.out));
     }
 }
