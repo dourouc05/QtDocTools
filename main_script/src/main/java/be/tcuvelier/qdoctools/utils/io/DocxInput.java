afa47e48330afd03e7075852587217293d8481c3
Thibaut Cuvelier <dourouc05@gmail.com>
2019-04-13 23:35:40 +0200

    From DOCX: implement tables and pretty printing from scratch.

--- main_script/src/main/java/be/tcuvelier/qdoctools/utils/io/DocxInput.java
+++ main_script/src/main/java/be/tcuvelier/qdoctools/utils/io/DocxInput.java
@@ -5,15 +5,12 @@ import org.apache.poi.xwpf.usermodel.*;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class DocxInput {
     public static void main(String[] args) throws IOException, XMLStreamException {
@@ -23,7 +20,8 @@ public class DocxInput {
     private XWPFDocument doc;
     private XMLStreamWriter xmlStream;
 
-    private int currentLevel;
+    private int currentDepth;
+    private int currentSectionLevel;
 
     private static String docbookNS = "http://docbook.org/ns/docbook";
     private static String xlinkNS = "http://www.w3.org/1999/xlink";
@@ -32,46 +30,66 @@ public class DocxInput {
         doc = new XWPFDocument(new FileInputStream(filename));
     }
 
+    private void writeIndent() throws XMLStreamException {
+        xmlStream.writeCharacters("  ".repeat(currentDepth));
+    }
+
+    private void increaseIndent() {
+        currentDepth += 1;
+    }
+
+    private void decreaseIndent() {
+        currentDepth -= 1;
+    }
+
+    private void writeNewLine() throws XMLStreamException {
+        xmlStream.writeCharacters("\n");
+    }
+
     public String toDocBook() throws IOException, XMLStreamException {
         // Initialise the XML stream.
-        StringWriter stringWriter = new StringWriter();
-        xmlStream = XMLOutputFactory.newInstance().createXMLStreamWriter(stringWriter);
+        ByteArrayOutputStream writer = new ByteArrayOutputStream();
+        xmlStream = XMLOutputFactory.newInstance().createXMLStreamWriter(writer, "UTF-8");
 
-        // Wrap the writer inside an indenting proxy.
-        // Copied from https://ewernli.wordpress.com/2009/06/18/stax-pretty-printer/.
-        xmlStream = (XMLStreamWriter) Proxy.newProxyInstance(
-                XMLStreamWriter.class.getClassLoader(),
-                new Class[] {XMLStreamWriter.class},
-                new PrettyPrintHandler(xmlStream)
-        );
+        // Initialise counters.
+        currentDepth = 0;
+        currentSectionLevel = 0;
 
         // Generate the document: root, prefixes, content, then close the sections that should be.
-        xmlStream.writeStartDocument();
-        xmlStream.writeCharacters("\n");
-        xmlStream.writeStartElement("db", "article", docbookNS);
+        xmlStream.writeStartDocument("UTF-8", "1.0");
 
+        writeNewLine(); writeIndent();
+        xmlStream.writeStartElement("db", "article", docbookNS);
         xmlStream.setPrefix("db", docbookNS);
         xmlStream.writeNamespace("db", docbookNS);
         xmlStream.setPrefix("xlink", xlinkNS);
         xmlStream.writeNamespace("xlink", xlinkNS);
-        xmlStream.writeCharacters("\n");
+        increaseIndent();
 
-        currentLevel = 0;
+        writeNewLine();
+
         for (IBodyElement b: doc.getBodyElements()) {
             visit(b);
         }
 
-//        while (1 >= currentLevel) {
+//        while (1 >= currentSectionLevel) {
 //            xmlStream.writeEndElement(); // </db:section>
-//            currentLevel -= 1;
+//            currentSectionLevel -= 1;
 //        }
 
+        decreaseIndent(); // For consistency: this has no impact on the produced XML.
+        writeNewLine();
         xmlStream.writeEndElement();
         xmlStream.writeEndDocument();
 
+        if (currentDepth != 0) {
+            System.err.println("Reached the end of the document, but the indentation depth is not zero: " +
+                    "there is a bug! Current depth: " + currentDepth);
+        }
+
         // Generate the string from the stream.
-        String xmlString = stringWriter.getBuffer().toString();
-        stringWriter.close();
+        String xmlString = writer.toString();
+        writer.close();
         return xmlString;
     }
 
@@ -89,68 +107,86 @@ public class DocxInput {
 
     private void visitParagraph(XWPFParagraph p) throws XMLStreamException {
         if (p.getRuns().size() > 0) {
-            switch (p.getStyleID()) {
-                case "Title":
-                    visitDocumentTitle(p);
-                    break;
-                case "Heading1":
-                case "Heading2":
-                case "Heading3":
-                case "Heading4":
-                case "Heading5":
-                case "Heading6":
-                case "Heading7":
-                case "Heading8":
-                case "Heading9":
-                // TODO: Part, Chapter?
-                    visitSectionTitle(p);
-                    break;
-                // TODO: Note, etc.?
-                default:
-                    xmlStream.writeStartElement(docbookNS, "para");
-                    visitRuns(p.getRuns());
-                    xmlStream.writeEndElement(); // </db:para>
-                    break;
+            if (p.getStyleID() == null) {
+                visitNormalParagraph(p);
+            } else {
+                switch (p.getStyleID()) {
+                    case "Title":
+                        visitDocumentTitle(p);
+                        break;
+                    case "Heading1":
+                    case "Heading2":
+                    case "Heading3":
+                    case "Heading4":
+                    case "Heading5":
+                    case "Heading6":
+                    case "Heading7":
+                    case "Heading8":
+                    case "Heading9":
+                    // TODO: Part, Chapter?
+                        visitSectionTitle(p);
+                        break;
+                    // TODO: Note, etc.?
+                    default:
+                        visitNormalParagraph(p);
+                        break;
+                }
             }
         }
+    }
+
+    private void visitNormalParagraph(XWPFParagraph p) throws XMLStreamException {
+        writeIndent();
+        xmlStream.writeStartElement(docbookNS, "para");
+        visitRuns(p.getRuns());
+        xmlStream.writeEndElement(); // </db:para>
+        writeNewLine();
     }
 
     private void visitDocumentTitle(XWPFParagraph p) throws XMLStreamException {
         // Called only once, at tbe beginning of the document. This function is thus also responsible for the main
         // <db:info> tag.
-        currentLevel = 0;
+        currentSectionLevel = 0;
+
+        writeIndent();
         xmlStream.writeStartElement(docbookNS, "info");
+        increaseIndent();
+
+        writeNewLine();
+        writeIndent();
+
         xmlStream.writeStartElement(docbookNS, "title");
         visitRuns(p.getRuns());
-//        xmlStream.writeEndElement(); // </db:title>
+        xmlStream.writeEndElement(); // </db:title>
+        writeNewLine();
+
         // TODO: What about the abstract?
 
-
-        xmlStream.writeStartElement(docbookNS, "emphasis");
-        xmlStream.writeCharacters("bold");
-        xmlStream.writeEndElement();
-        xmlStream.writeEndElement();
-
+        decreaseIndent();
+        writeIndent();
         xmlStream.writeEndElement(); // </db:info>
+        writeNewLine();
     }
 
     private void visitSectionTitle(XWPFParagraph p) throws XMLStreamException {
         // Pop sections until the current level is reached.
         int level = Integer.parseInt(p.getStyleID().replace("Heading", ""));
-        while (level >= currentLevel) {
+        while (level >= currentSectionLevel) {
             xmlStream.writeEndElement(); // </db:section>
-            currentLevel -= 1;
+            currentSectionLevel -= 1;
         }
 
+        // TODO: Indent.
+
         // Deal with this section.
-        currentLevel += 1;
+        currentSectionLevel += 1;
         xmlStream.writeStartElement(docbookNS, "section");
         xmlStream.writeStartElement(docbookNS, "title");
         visitRuns(p.getRuns());
         xmlStream.writeEndElement(); // </db:title>
         xmlStream.writeCharacters("\n");
 
-        // TODO: Implement a check on the currentLevel and the level (in case someone missed a level in the headings).
+        // TODO: Implement a check on the currentSectionLevel and the level (in case someone missed a level in the headings).
     }
 
     private void visitRuns(List<XWPFRun> runs) throws XMLStreamException {
@@ -216,75 +252,53 @@ public class DocxInput {
     }
 
     private void visitTable(XWPFTable t) throws XMLStreamException {
+        writeIndent();
         xmlStream.writeStartElement(docbookNS, "informaltable");
+        increaseIndent();
+        writeNewLine();
 
         // Output the table row per row, in HTML format.
         for (XWPFTableRow row: t.getRows()) {
+            writeIndent();
             xmlStream.writeStartElement(docbookNS, "tr");
+            increaseIndent();
+            writeNewLine();
 
             for (XWPFTableCell cell: row.getTableCells()) {
+                // Special case: an empty cell. One paragraph with zero runs.
+                if (cell.getParagraphs().size() == 0 ||
+                        (cell.getParagraphs().size() == 1 && cell.getParagraphs().get(0).getRuns().size() == 0)) {
+                    writeIndent();
+                    xmlStream.writeEmptyElement(docbookNS, "td");
+                    writeNewLine();
+
+                    continue;
+                }
+
+                // Normal case.
+                writeIndent();
                 xmlStream.writeStartElement(docbookNS, "td");
+                increaseIndent();
+                writeNewLine();
+
+                for (XWPFParagraph p: cell.getParagraphs()){
+                    visitParagraph(p);
+                }
+
+                decreaseIndent();
+                writeIndent();
                 xmlStream.writeEndElement(); // </db:td>
+                writeNewLine();
             }
 
+            decreaseIndent();
+            writeIndent();
             xmlStream.writeEndElement(); // </db:tr>
+            writeNewLine();
         }
 
+        decreaseIndent();
+        writeIndent();
         xmlStream.writeEndElement(); // </db:informaltable>
-    }
-
-    // Copied from https://ewernli.wordpress.com/2009/06/18/stax-pretty-printer/
-    private static class PrettyPrintHandler implements InvocationHandler {
-        private final XMLStreamWriter target;
-        private int depth = 0;
-        private final Map<Integer, Boolean> hasChildElement = new HashMap<>();
-        private static final String INDENT = " ";
-        private static final String LINEFEED = "\n";
-
-        public PrettyPrintHandler(XMLStreamWriter t) {
-            target = t;
-        }
-
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            String m = method.getName();
-
-            // Needs to be BEFORE the actual event, so that for instance the
-            // sequence writeStartElem, writeAttr, writeStartElem, writeEndElem, writeEndElem
-            // is correctly handled
-            if ("writeStartElement".equals(m)) {
-                // update state of parent node
-                if (depth > 0) {
-                    hasChildElement.put(depth - 1, true);
-                }
-                // reset state of current node
-                hasChildElement.put(depth, false);
-                // indent for current depth
-                target.writeCharacters(LINEFEED);
-                target.writeCharacters(repeat(depth));
-                depth++;
-            }
-            else if ("writeEndElement".equals(m)) {
-                depth--;
-                if (hasChildElement.get(depth)) {
-                    target.writeCharacters(LINEFEED);
-                    target.writeCharacters(repeat(depth));
-                }
-            }
-            else if ("writeEmptyElement".equals(m)) {
-                // update state of parent node
-                if (depth > 0) {
-                    hasChildElement.put(depth - 1, true);
-                }
-                // indent for current depth
-                target.writeCharacters(LINEFEED);
-                target.writeCharacters(repeat(depth));
-            }
-            method.invoke(target, args);
-            return null;
-        }
-
-        private static String repeat(int d) {
-            return PrettyPrintHandler.INDENT.repeat(d);
-        }
     }
 }
