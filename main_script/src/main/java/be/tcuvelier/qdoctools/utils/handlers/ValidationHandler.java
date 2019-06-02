b8ad6b86137095ed555667fa5d0d2b8a3d507f49
Thibaut Cuvelier <dourouc05@gmail.com>
2019-06-02 04:04:55 +0200

    Refactor the validation handlers to be more useable from outside their initial use case (more code, less redundancy: who wins?).

--- main_script/src/main/java/be/tcuvelier/qdoctools/utils/handlers/ValidationHandler.java
+++ main_script/src/main/java/be/tcuvelier/qdoctools/utils/handlers/ValidationHandler.java
@@ -12,6 +12,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class ValidationHandler {
+    private static boolean subValidate(File file, Schema schema) throws IOException {
+        return subValidate(file, schema.newValidator());
+    }
+
     private static boolean subValidate(File file, Validator validator) throws IOException {
         try {
             validator.validate(new StreamSource(file)); // SAXException when validation error.
@@ -22,23 +26,42 @@ public class ValidationHandler {
         }
     }
 
-    public static boolean validateRNG(File file, String rng) throws SAXException, IOException {
+    private static void loadJing() {
         // Required according to https://pages.lip6.fr/Jean-Francois.Perrot/XML-Int/Session3/RNG/.
         System.setProperty(
                 SchemaFactory.class.getName() + ":" + XMLConstants.RELAXNG_NS_URI,
                 "com.thaiopensource.relaxng.jaxp.CompactSyntaxSchemaFactory"
         );
-        SchemaFactory factory = CompactSyntaxSchemaFactory.newInstance(XMLConstants.RELAXNG_NS_URI);
-        Schema schema = factory.newSchema(new StreamSource(new File(rng)));
-        Validator validator = schema.newValidator();
-        return subValidate(file, validator);
+    }
+
+    private static Schema loadSchema(String schema, String format) throws SAXException {
+        SchemaFactory factory = SchemaFactory.newInstance(format);
+        return factory.newSchema(new StreamSource(new File(schema)));
+    }
+
+    public static Schema loadRNGSchema(String rng) throws SAXException {
+        try {
+            return unsafeLoadRNGSchema(rng);
+        } catch (SAXException e) {
+            // Try a second time, forcing to load Jing.
+            loadJing();
+            return unsafeLoadRNGSchema(rng);
+        }
+    }
+
+    private static Schema unsafeLoadRNGSchema(String rng) throws SAXException {
+        return loadSchema(rng, XMLConstants.RELAXNG_NS_URI);
+    }
+
+    public static Schema loadXSDSchema(String xsd) throws SAXException {
+        return loadSchema(xsd, XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    }
+
+    public static boolean validateRNG(File file, String rng) throws SAXException, IOException {
+        return subValidate(file, loadRNGSchema(rng));
     }
 
     public static boolean validateXSD(File file, String xsd) throws SAXException, IOException {
-        SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
-        Schema schema = factory.newSchema(new StreamSource(new File(xsd)));
-
-        Validator validator = schema.newValidator();
-        return subValidate(file, validator);
+        return subValidate(file, loadXSDSchema(xsd));
     }
 }
