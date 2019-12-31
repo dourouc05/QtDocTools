00172477a5771a9165aa06534788ddd06bb97af8
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2019-12-31 14:57:04 +0100

    Start implementing upload.

--- main_script/src/main/java/be/tcuvelier/qdoctools/core/UploadCore.java
+++ main_script/src/main/java/be/tcuvelier/qdoctools/core/UploadCore.java
@@ -1,10 +1,24 @@
 package be.tcuvelier.qdoctools.core;
 
+import be.tcuvelier.qdoctools.core.handlers.DvpToolchainHandler;
+import be.tcuvelier.qdoctools.core.utils.Configuration;
+import net.sf.saxon.s9api.SaxonApiException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class UploadCore {
-    public static void call(String input, String folder, boolean upload, String configurationFile) throws Exception {
+    public static void call(String input, String folder, boolean upload, String configurationFile)
+            throws IOException, SaxonApiException, InterruptedException {
+        Configuration config = new Configuration(configurationFile);
+
         // Find the configuration file for the operation to perform (contains info about uploading).
 
         // Perform generation with Dvp toolchain.
+        String output = folder.isEmpty() ? Files.createTempDirectory("qdt").toString() : folder;
+        DvpToolchainHandler.generateHTML(input, output, config);
 
         // Upload if required
         if (upload) {
