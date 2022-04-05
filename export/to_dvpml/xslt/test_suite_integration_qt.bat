a7c1a7e4804d83282f869107a810b584eb56030f
Thibaut Cuvelier <dourouc05@gmail.com>
2022-04-05 04:40:17 +0200

    DB to DvpML: adapt test case to remove attributes that should not appear.

--- export/to_dvpml/xslt/test_suite_integration_qt.bat
+++ export/to_dvpml/xslt/test_suite_integration_qt.bat
@@ -6,6 +6,8 @@ set SAXON=saxon-he-11.3.jar
 for %%f in (test\*.qdt) do (
     echo %%~nf
     java -jar "%SAXON%" "doc-qt=true" "document-file-name=%%~nf" "section=65" -s:test\%%~nf.qdt -xsl:docbook_to_dvpml.xslt
+    rem exit /B
+    echo .
 )
 
 pause
