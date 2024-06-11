e2fdbd45c50bb59a24b535a57e352bd594c2c5dd
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2024-06-11 02:20:51 +0200

    Register the new command.

--- main_script/src/main/java/be/tcuvelier/qdoctools/cli/MainCommand.java
+++ main_script/src/main/java/be/tcuvelier/qdoctools/cli/MainCommand.java
@@ -10,7 +10,8 @@ import java.util.concurrent.Callable;
         TransformCommand.class,
         MergeCommand.class,
         UploadCommand.class,
-        QDocCommand.class
+        QDocCommand.class,
+        QDocPublishCommand.class
 }, mixinStandardHelpOptions = true, version = "QDocTools 0.1.0")
 public class MainCommand implements Callable<Void> {
     @CommandLine.Spec
