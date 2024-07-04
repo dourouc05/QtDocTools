10f525dc563396149a3d65f2a9e72845a074ff03
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2024-07-04 23:08:02 +0200

    Slight cleanup.

--- main_script/src/main/java/be/tcuvelier/qdoctools/core/QDocPostProcessCore.java
+++ main_script/src/main/java/be/tcuvelier/qdoctools/core/QDocPostProcessCore.java
@@ -3,8 +3,6 @@ package be.tcuvelier.qdoctools.core;
 import be.tcuvelier.qdoctools.core.config.GlobalConfiguration;
 import be.tcuvelier.qdoctools.core.handlers.QDocMovingHandler;
 import be.tcuvelier.qdoctools.core.handlers.QDocPostProcessingHandler;
-import be.tcuvelier.qdoctools.core.handlers.QDocRunningHandler;
-import be.tcuvelier.qdoctools.core.utils.QtVersion;
 import net.sf.saxon.s9api.SaxonApiException;
 import org.xml.sax.SAXException;
 
@@ -22,18 +20,21 @@ public class QDocPostProcessCore {
         List<String> includes = config.getCppCompilerIncludes();
         includes.addAll(config.getNdkIncludes());
 
-        QDocMovingHandler qrh = new QDocMovingHandler(outputFolder);
-        QDocPostProcessingHandler qpph = new QDocPostProcessingHandler(outputFolder, htmlFolder, config);
+        {
+            System.out.println("++> Copying QDoc output.");
+            QDocMovingHandler qrh = new QDocMovingHandler(outputFolder);
+            qrh.copyGeneratedFiles(); // Sometimes, qdoc outputs things in a strange folder. Ahoy!
+            System.out.println("++> QDoc output copied.");
+        }
 
-        System.out.println("++> Copying QDoc output.");
-        qrh.copyGeneratedFiles(); // Sometimes, qdoc outputs things in a strange folder. Ahoy!
-        System.out.println("++> QDoc output copied.");
-
-        System.out.println("++> Fixing some qdoc quirks.");
-        qpph.fixQDocBugs();
-        qpph.addDates();
-        qpph.addAuthors();
-        qpph.fixLinks();
-        System.out.println("++> QDoc quirks fixed."); // At least, the ones I know about right now.
+        {
+            System.out.println("++> Fixing some qdoc quirks.");
+            QDocPostProcessingHandler qpph = new QDocPostProcessingHandler(outputFolder, htmlFolder, config);
+            qpph.fixQDocBugs();
+            qpph.addDates();
+            qpph.addAuthors();
+            qpph.fixLinks();
+            System.out.println("++> QDoc quirks fixed."); // At least, the ones I know about right now.
+        }
     }
 }
