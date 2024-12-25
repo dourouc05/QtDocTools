158ab4d31e7ba2353393005e7e9ed894b22e3cdb
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2024-12-26 00:10:34 +0100

    Remove unused imports.

--- main_script/src/main/java/be/tcuvelier/qdoctools/core/handlers/QDocFixHandler.java
+++ main_script/src/main/java/be/tcuvelier/qdoctools/core/handlers/QDocFixHandler.java
@@ -6,12 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 // Handler for fixing QDoc's DocBook output.
 //
